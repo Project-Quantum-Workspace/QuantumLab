b5fb805b0e985e801d703ba9e43889192bd71788
Xiaochen Hou <houxiaochen01@gmail.com>
2023-08-13 00:28:36 +1000

    fix: update the template request logic

--- website/src/pages/NewWorkspace/index.tsx
+++ website/src/pages/NewWorkspace/index.tsx
@@ -17,49 +17,51 @@ const { Option } = Select;
 const NewWorkspaceForm = () => {
   const [form] = Form.useForm();
 
-  type ParametersType = {
-    [key: string]: {
-      fieldType: 'opt' | 'num';
-      options?: string[];
-    };
+  type Question = {
+    name: string;
+    label: string;
+    selections?: string[];
+    isInput: boolean;
   };
 
   type Template = {
-    Id: number;
-    Parameters: Record<string, any>; // Add this type definition
-    Access_level: string;
-    File_name: string;
+    accessLevel: string;
+    filename: string;
+    parameters: Question[];
+    id: number; // Assuming an ID is part of the template
   };
 
   const [selectedTemplate, setSelectedTemplate] = useState<Template | null>(null);
 
   const [templates, setTemplates] = useState<Template[]>([]);
+  const [loadingTemplates, setLoadingTemplates] = useState(true);
+  const [templatesFetchFailed, setTemplatesFetchFailed] = useState(false);
 
   useEffect(() => {
     // function to fetch templates
     const fetchTemplates = async () => {
       try {
-        const response = await fetch('http://localhost:8080/api/template/get', {
+        setLoadingTemplates(true);
+        const response = await fetch('/api/templates', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
           },
         });
-
         const data = await response.json();
 
         if (response.ok) {
-          setTemplates(data.message); // set fetched templates to state
+          console.log('data:', data);
+          setTemplates(data);
+          console.log('templates:', templates);
         } else {
           throw new Error(data.message || 'Error fetching templates.');
         }
+        setLoadingTemplates(false);
       } catch (error) {
         console.error('Error:', error);
-        notification.error({
-          message: 'Error',
-          description: 'An error occurred while fetching templates.',
-          duration: 5,
-        });
+        setTemplatesFetchFailed(true);
+        setLoadingTemplates(false);
       }
     };
 
@@ -67,41 +69,73 @@ const NewWorkspaceForm = () => {
     fetchTemplates();
   }, []);
 
-  const onTemplateChange = (value: number) => {
-    const template = templates.find((template) => template.Id === value);
-    if (template && typeof template.Parameters === 'string') {
-      // Parse the Parameters from string to object
-      const paramsObject = JSON.parse(template.Parameters);
+  if (loadingTemplates) {
+    return <div>Loading templates...</div>;
+  }
 
-      setSelectedTemplate({ ...template, Parameters: paramsObject });
+  if (templatesFetchFailed) {
+    return (
+      <div style={{ padding: '20px' }}>
+        <Title type="danger">An error occurred while fetching templates. Please try again.</Title>
+        <Button
+          type="primary"
+          onClick={() => window.location.reload()}
+          style={{ marginLeft: '10px' }}
+        >
+          Refresh Page
+        </Button>
+      </div>
+    );
+  }
+
+  const onTemplateChange = (selectedTemplateId: number) => {
+    const template = templates.find((template) => template.id === selectedTemplateId);
+    if (template && typeof template.parameters === 'string') {
+      // Parse the Parameters from string to object
+      const paramsObject = JSON.parse(template.parameters);
+      setSelectedTemplate({ ...template, parameters: paramsObject });
     }
   };
 
   const onFinish = async (values: any) => {
     try {
+      const parameters = selectedTemplate?.parameters.reduce<Record<string, any>>(
+        (acc, question) => {
+          acc[question.name] = values[question.name];
+          return acc;
+        },
+        {},
+      );
+
       // Adjust data to fit the new format
       const adjustedValues = {
-        createdAt: new Date().toISOString(),
-        description: values.description,
-        id: values.id,
-        lastAccessed: new Date().toISOString(),
-        name: values.name,
-        parameters: values.parameters,
-        state: values.state,
-        tags: values.tags.split(',').map((tag: string) => tag.trim()),
-        template_id: values.template_id,
-        type: values.type,
-        updatedAt: new Date().toISOString(),
-        user_id: values.user_id,
+        userId: 1, // or any other user ID you need to set
+        workspace: {
+          createdAt: new Date().toISOString(),
+          description: values.description,
+          id: 111,
+          lastAccessed: new Date().toISOString(),
+          name: values.name,
+          parameters: JSON.stringify(parameters), // Stringify the parameters
+          state: values.state,
+          tags: values.tags
+            .split(',')
+            .map((tag: string) => tag.trim())
+            .join(','),
+          templateId: values.template_id,
+          type: values.type,
+          updatedAt: new Date().toISOString(),
+        },
       };
 
-      const response = await fetch('http://localhost:8080/workspace/create', {
+      console.log('adjustedValues:', adjustedValues);
+
+      const response = await fetch('/api/workspaces', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(adjustedValues),
-        // Removed 'no-cors' mode, because we need to handle response body
       });
 
       const data = await response.json();
@@ -133,7 +167,6 @@ const NewWorkspaceForm = () => {
       }
     } catch (error) {
       console.error('Fetch Error:', error);
-
       notification.error({
         message: 'Error',
         description: 'An error occurred while processing your request.',
@@ -186,75 +219,49 @@ const NewWorkspaceForm = () => {
         </Form.Item>
 
         <Form.Item name="template_id" label="Templates">
-          <Select placeholder="Select a template" onChange={onTemplateChange}>
-            {templates.map((template) => (
-              <Option key={template.Id} value={template.Id}>
-                {template.File_name}
-              </Option>
-            ))}
+          <Select
+            placeholder="Select a template"
+            onChange={onTemplateChange}
+            loading={!templates} // Show loading indicator if templates are not available
+          >
+            {templates ? (
+              templates.map((template) => (
+                <Option key={template.id} value={template.id}>
+                  {template.filename}
+                </Option>
+              ))
+            ) : (
+              <Option disabled>Loading templates...</Option>
+            )}
           </Select>
         </Form.Item>
 
         {selectedTemplate &&
-          selectedTemplate.Parameters &&
-          Object.keys(selectedTemplate.Parameters).map((key) => (
+          selectedTemplate.parameters &&
+          selectedTemplate.parameters.map((question, index) => (
             <>
-              <Divider />
-              <Title level={4}>Workspace Parameters</Title>
-              <Form.Item name={key} label={key}>
-                {selectedTemplate.Parameters[key].fieldType === 'opt' ? (
-                  <Select>
-                    {selectedTemplate.Parameters[key].options.map(
-                      (option: string, index: number) => (
-                        <Option key={index} value={option}>
-                          {option}
-                        </Option>
-                      ),
-                    )}
-                  </Select>
+              {index === 0 && (
+                <>
+                  <Divider />
+                  <Title level={4}>Workspace Parameters</Title>
+                </>
+              )}
+              <Form.Item name={question.name} label={question.label}>
+                {question.isInput ? (
+                  <InputNumber min={0} step={1} />
                 ) : (
-                  <InputNumber />
+                  <Select>
+                    {question.selections?.map((option: string, index: number) => (
+                      <Option key={index} value={option}>
+                        {option}
+                      </Option>
+                    ))}
+                  </Select>
                 )}
               </Form.Item>
             </>
           ))}
 
-        {/* <Form.Item name="region" label="What region should your workspace deploy in?">
-          <Select>
-            <Option value="qh2-uom">qh2-uom</Option>
-            <Option value="qh2-uom2">qh2-uom2</Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item label="What instance flavor should your workspace use?" name="flavor">
-          <Select>
-            <Option value="2 Cores CPU + 2G Memory(t3.small)">
-              2 Cores CPU + 2G Memory(t3.small)
-            </Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item label="Which OS should your workspace use?" name="os">
-          <Select>
-            <Option value="NeCTAR Ubuntu 20.04 LTS (Focal) amd64 Helping text is here">
-              NeCTAR Ubuntu 20.04 LTS (Focal) amd64 Helping text is here
-            </Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item label="Install JupyterLab?" name="jupyter">
-          <Select>
-            <Option value="true">true</Option>
-            <Option value="false">false</Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item
-          label="How large would you like your persistent home volume to be (Gi)"
-          name="volume"
-        >
-          <InputNumber min={1} step={1} max={100} />
-        </Form.Item> */}
         <Form.Item>
           <Button type="primary" htmlType="submit">
             Submit
