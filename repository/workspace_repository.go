47927c7f057dcff6861276e110fb3ab61a2e2709
StevenDB <xieyijie98@gmail.com>
2023-08-13 12:52:53 +1000

    fix[workspace]: minor fix

--- repository/workspace_repository.go
+++ repository/workspace_repository.go
@@ -19,7 +19,8 @@ func NewWorkspaceRepository(qlDB *gorm.DB) model.WorkspaceRepository {
 func (repo *workspaceRepository) Create(workspace *model.Workspace, userID uint) error {
 	err := repo.qlDB.Transaction(func(tx *gorm.DB) error {
 		// Omit `ID` to avoid error triggered by frontend developers adding id field in requests
-		result := tx.Omit("ID").Create(workspace)
+		// Omit `Template` to forbid auto create of Templates
+		result := tx.Omit("ID", "Template").Create(workspace)
 		if result.Error != nil {
 			return result.Error
 		}
@@ -58,7 +59,7 @@ func (repo *workspaceRepository) Update(workspace *model.Workspace) error {
 	// Omit `Users.*` allows insertions of new user_workspaces associations
 	// for possible shared workspace extension
 	result := repo.qlDB.Model(workspace).
-		Omit("ID", "Template", "Users.*").Updates(*workspace)
+		Omit("ID", "Template").Updates(*workspace)
 	return result.Error
 }
 
