3d56bd9c491b8923f00b1d57fb2f618b51dcd736
Edward Lin <edward.lin1695@hotmail.com>
2023-08-19 16:55:19 +1000

    feat[middleware]: integrate authentication middleware and private/public routing

--- api/route/route.go
+++ api/route/route.go
@@ -1,19 +1,20 @@
 package route
 
 import (
+	"github.com/Project-Quantum-Workspace/QuantumLab/api/middleware"
 	"github.com/Project-Quantum-Workspace/QuantumLab/bootstrap"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func Setup(env *bootstrap.Env, db *gorm.DB, rdb *gorm.DB, engine *gin.Engine) {
-	apiRouterGroup := engine.Group("/api")
+func Setup(env *bootstrap.Env, db *gorm.DB, engine *gin.Engine) {
+	publicApiRouterGroup := engine.Group("/api")
+	NewLoginRouter(env, db, publicApiRouterGroup)
 
-	NewLoginRouter(env, db, apiRouterGroup)
-	NewUserAdminRouter(env, db, apiRouterGroup)
-
-	NewTemplateRouter(db, apiRouterGroup)
-	NewWorkspaceRouter(db, apiRouterGroup)
-	NewResultRouter(rdb, apiRouterGroup)
+	privateApiRouterGroup := engine.Group("/api")
+	privateApiRouterGroup.Use(middleware.JwtAuthenticator(env.AccessJWTSecret))
+	NewUserAdminRouter(env, db, privateApiRouterGroup)
+	NewTemplateRouter(db, privateApiRouterGroup)
+	NewWorkspaceRouter(db, privateApiRouterGroup)
 }
