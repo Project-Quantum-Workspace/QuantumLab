9e5f887ff715f2d7bdd929bd9b39bd5515177ab7
Zixuan Liu <2587449765@qq.com>
2023-08-25 20:18:45 +1000

    fix[result]: fixed the merge conflict

--- api/route/route.go
+++ api/route/route.go
@@ -8,13 +8,14 @@ import (
 	"gorm.io/gorm"
 )
 
-
 func Setup(env *bootstrap.Env, db *gorm.DB, rdb *gorm.DB, engine *gin.Engine) {
-	apiRouterGroup := engine.Group("/api")
-	NewLoginRouter(env, db, apiRouterGroup)
-	NewSignupRouter(db, apiRouterGroup)
+	publicApiRouterGroup := engine.Group("/api")
+	NewLoginRouter(env, db, publicApiRouterGroup)
+	NewSignupRouter(db, publicApiRouterGroup)
+	privateApiRouterGroup := engine.Group("/api")
+	privateApiRouterGroup.Use(middleware.JwtAuthenticator(env.AccessJWTSecret))
 	//template router
-	TemplateRouter(db, apiRouterGroup)
-	NewWorkspaceRouter(db, apiRouterGroup)
-	NewResultRouter(rdb, apiRouterGroup)
+	TemplateRouter(db, privateApiRouterGroup)
+	NewWorkspaceRouter(db, privateApiRouterGroup)
+	NewResultRouter(rdb, privateApiRouterGroup)
 }
