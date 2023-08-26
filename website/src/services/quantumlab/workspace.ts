ecddae2560a1ce4b1110e428446e9f09461a484c
StevenDB <xieyijie98@gmail.com>
2023-08-27 01:46:51 +1000

    Revert "fix:change id"
    This reverts commit 137ca22c1ff1344005b5865534b21c6e039d6b4e.

--- website/src/services/quantumlab/workspace.ts
+++ website/src/services/quantumlab/workspace.ts
@@ -1,7 +1,7 @@
 import { request } from '@umijs/max';
 
 export async function getWorkspace(id: string) {
-  return request('http://localhost:8080/api/workspaces/' + id, {
+  return request('/api/workspaces/' + id, {
     method: 'GET',
     // headers: {
     //   Authorization: `Bearer ${token}`,
