d78a16591edd9fd9fe5156516a94148793c82600
StevenDB <xieyijie98@gmail.com>
2023-08-27 01:46:05 +1000

    Revert "feat[middleware]: integrate authentication middleware and private/public routing"
    This reverts commit 3d56bd9c491b8923f00b1d57fb2f618b51dcd736.

--- api/route/route.go
+++ api/route/route.go
@@ -1,20 +1,19 @@
 package route
 
 import (
-	"github.com/Project-Quantum-Workspace/QuantumLab/api/middleware"
 	"github.com/Project-Quantum-Workspace/QuantumLab/bootstrap"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func Setup(env *bootstrap.Env, db *gorm.DB, engine *gin.Engine) {
-	publicApiRouterGroup := engine.Group("/api")
-	NewLoginRouter(env, db, publicApiRouterGroup)
+func Setup(env *bootstrap.Env, db *gorm.DB, rdb *gorm.DB, engine *gin.Engine) {
+	apiRouterGroup := engine.Group("/api")
 
-	privateApiRouterGroup := engine.Group("/api")
-	privateApiRouterGroup.Use(middleware.JwtAuthenticator(env.AccessJWTSecret))
-	NewUserAdminRouter(env, db, privateApiRouterGroup)
-	NewTemplateRouter(db, privateApiRouterGroup)
-	NewWorkspaceRouter(db, privateApiRouterGroup)
+	NewLoginRouter(env, db, apiRouterGroup)
+	NewUserAdminRouter(env, db, apiRouterGroup)
+
+	NewTemplateRouter(db, apiRouterGroup)
+	NewWorkspaceRouter(db, apiRouterGroup)
+	NewResultRouter(rdb, apiRouterGroup)
 }
