f76d3bbbacd107e07351ac5fcb9e8b5752fa310c
StevenDB <xieyijie98@gmail.com>
2023-08-27 01:46:34 +1000

    Revert "feat: disable mock"
    This reverts commit 7307f2ff28f11802272bb7747bbaf527e7370e87.

--- website/mock/listWorkspaceTable.ts
+++ website/mock/listWorkspaceTable.ts
@@ -1,23 +1,18 @@
-import { TemplateMetaData } from '@/pages/Workspace/utils/types/TemplateTypes';
 import { Request, Response} from 'express';
 
 import moment  from 'moment';
 
 
 type WorkspaceInfoMetaData={
-  id?: number;
-  name?: string;
-  createdAt?: string;
-  lastAccessed?: string;
-  updatedAt?: string;
-  description?: string;
-  templateId?: number;
-  template?: TemplateMetaData;
-  state?: string;
-  tags?: string;
-  type?: string;
-  parameters?: string;
-  message?: string;
+    id: number,
+    name: string,
+    createdAt: string,
+    lastAccessed:string,
+    updatedAt: string,
+    description:string,
+    templateId:number,
+    state:string,
+    tags:string
 }
 const stutusValue = ['Stopped', 'Pending', 'Running', 'Failed', 'Connecting']
 const genWorkspaceList = (current:number, pageSize:number)=>{
@@ -30,10 +25,9 @@ const genWorkspaceList = (current:number, pageSize:number)=>{
             createdAt: "2023-08-10T15:04:05Z",
             lastAccessed: moment().format('YYYY-MM-DD'),
             updatedAt: moment().format('YYYY-MM-DD'),
-            description: "this is an example project",
+            description: "",
             templateId: 1,
             state: stutusValue[i],
-            parameters: '{"Disk Size": "50G", "Available Zone": "qh2-uom"}',
             tags: 'CPU, Qiskit',
   
         })
