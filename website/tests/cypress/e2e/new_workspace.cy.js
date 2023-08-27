933e242ff943578773d486b724c15657cbf1bb0f
lois-ekd81194 <yifewang2@student.unimelb.edu.au>
2023-08-27 20:37:04 +1000

    test[new_workspace]: updated test casefor selecting template

--- website/tests/cypress/e2e/new_workspace.cy.js
+++ website/tests/cypress/e2e/new_workspace.cy.js
@@ -1,8 +1,6 @@
 describe('New a Workspace Form', () => {
   beforeEach(() => {
-    
     cy.autoLogin('workspacequantum@gmail.com','workspacequantum@gmail.com')
-    
     cy.visit(`${Cypress.env('QUANTUMLAB_WEB')}/workspace/new`)
   })
   
@@ -12,13 +10,15 @@ describe('New a Workspace Form', () => {
     cy.get('#description').type("a example test")
     cy.get('.ant-btn-primary').click()
     cy.get('.ant-notification-notice-message').should('exist')
+    //TODO: verify the notification message
+
+    //TODO: notification vanished after 5s
   })
   it("Show error message when user submit without entering information", () => {
    
     cy.get('.ant-btn-primary').click()
     cy.get('#name_help > .ant-form-item-explain-error').should('exist')
   })
-  
   
   it("Allows users to submit form with properly filled information",()=>{
     cy.get('#name').type("test")
@@ -28,7 +28,17 @@ describe('New a Workspace Form', () => {
     cy.get('.ant-select-item-option-active > .ant-select-item-option-content').click()
     cy.get('.ant-btn-primary').click()
     cy.get('.ant-notification-notice').should('exist')
+    //TODO: verify the notification message
+
+    //TODO: notification vanished after 5s
   
+  })
+  it("Template form is generated",()=>{
+    cy.get('.ant-select-selector').click()
+    cy.get('.ant-select-item-option-active > .ant-select-item-option-content').click()
+    cy.get('#availableZone')
+    cy.get('#diskSize')
+    
   })
   it("Allows users to go back to workspace page",()=>{
     cy.get('a > .ant-btn').click()
