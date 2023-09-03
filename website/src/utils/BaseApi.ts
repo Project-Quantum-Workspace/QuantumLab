ed5681f67a7bb11ca41211b5853d0333d9b7dc4f
liuxt805 <xintongl13@student.unimelb.edu.au>
2023-09-03 20:40:40 +1000

    feat: add BaseApi for http requests

---
+++ website/src/utils/BaseApi.ts
@@ -0,0 +1,48 @@
+import { request } from '@umijs/max';
+import type { AxiosError, Method, ResponseType } from 'axios'
+
+export class BaseApi {
+  private loadBy(
+    url: string,
+    method: Method,
+    data?: Object,
+    responseType: ResponseType = 'json',
+    withCredentials: boolean = true,
+    headers?: Record<string, string>
+  ) {
+    return request(url, {
+      method,
+      data,
+      responseType,
+      withCredentials,
+      headers
+    }).then((response) => response)
+    .catch((error: AxiosError) => { throw error });
+  }
+
+  loadByGet(
+    url: string, 
+    withCredentials?: boolean,
+    headers?: Record<string, string>
+  ) {
+    return this.loadBy(url, 'GET', undefined, undefined, withCredentials, headers)
+  }
+
+  loadByPost(
+    url: string, 
+    data?: Object,
+    responseType?: ResponseType,
+    withCredentials?: boolean,
+    headers?: Record<string, string>
+  ) {
+    return this.loadBy(url, 'POST', data, responseType, withCredentials, headers)
+  }
+
+  loadByDelete(url: string) {
+    return this.loadBy(url, 'DELETE')
+  }
+
+  loadbyPatch(url: string, data?: Object) {
+    return this.loadBy(url, 'PATCH', data)
+  }
+}
