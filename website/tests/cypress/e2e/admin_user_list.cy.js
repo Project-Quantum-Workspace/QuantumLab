5cd24a633325000ad079bf8610c802a8e39103b4
Xiaochen Hou <houxiaochen01@gmail.com>
2023-09-05 23:00:53 +1000

    update: new tests for admin user list

---
+++ website/tests/cypress/e2e/admin_user_list.cy.js
@@ -0,0 +1,85 @@
+
+function loginThroughUI(email, password) {
+  // // Visit the login page
+  cy.visit('http://localhost:8000/login');  // Adjust this if your login URL is different
+
+  cy.get('#email').type(email);  // Adjusted for your provided email input
+
+  // Assuming a similar structure for the password field, adjust if different
+  cy.get('#password').type(password);
+
+  // Click the login button
+  cy.get('.ant-btn.ant-btn-primary.ant-btn-lg').click();
+
+  // Wait and verify that the login was successful, for example by checking for a specific element that appears only after login
+  // cy.get('.logged-in-indicator').should('be.visible');  // Adjust this selector
+  cy.visit('http://localhost:8000/home');
+}
+
+describe('Admin User List', () => {
+  beforeEach(() => {
+    // loginThroughUI('workspacequantum@gmail.com','workspacequantum@gmail.com')
+    cy.autoLogin('workspacequantum@gmail.com', 'workspacequantum@gmail.com')
+    // Set up stubs
+    cy.intercept('GET', `${Cypress.env('API_LINK')}/admin/users`, { fixture: 'users.json' }).as('fetchUsers');
+    cy.intercept('PATCH', `${Cypress.env('API_LINK')}/admin/users`, {}).as('setStatus');
+
+    cy.visit(`${Cypress.env('QUANTUMLAB_WEB')}/admin/users`)
+    // Wait for the users to load
+    cy.wait('@fetchUsers');
+  })
+
+  it('should load user data correctly', () => {
+    cy.get('table').should('contain', 'Jane Doe').and('contain', 'Admin').and('contain', 'Active');
+    cy.get('table').should('contain', 'John Doe').and('contain', 'User').and('contain', 'Inactive');
+  });
+
+  it('shows error notification when fetching users fails', () => {
+    cy.intercept('GET', '/api/admin/users', {
+      statusCode: 500
+    }).as('failedFetch');
+
+    cy.visit('/admin/user-list');
+    cy.wait('@failedFetch');
+
+    cy.get('body').should('contain', 'Error Fetching Users');
+  });
+
+  it('updates multiple users to inactive and shows a notification', () => {
+    // Select Jane and John
+    cy.get('table').find('tbody > tr:nth-child(1)').find('td input[type="checkbox"]').click();
+    cy.get('table').find('tbody > tr:nth-child(2)').find('td input[type="checkbox"]').click();
+
+    // Set them to Inactive
+    cy.get('button').contains('Set Inactive').click();
+    cy.wait('@setStatus').its('request.body').should('deep.include', {
+      // Check if the correct uuids are being sent (this might need to be adjusted)
+    });
+
+    // The users should be updated to Inactive
+    cy.get('table').find('tbody > tr:nth-child(1)').should('contain', 'Inactive');
+    cy.get('table').find('tbody > tr:nth-child(2)').should('contain', 'Inactive');
+
+    // A success notification should be shown
+    cy.get('body').should('contain', 'Update Successful');
+  });
+
+  it('updates multiple users to active and shows a notification', () => {
+    // Select Jane and John
+    cy.get('table').find('tbody > tr:nth-child(1)').find('td input[type="checkbox"]').click();
+    cy.get('table').find('tbody > tr:nth-child(2)').find('td input[type="checkbox"]').click();
+
+    // Set them to Active
+    cy.get('button').contains('Set Active').click();
+    cy.wait('@setStatus').its('request.body').should('deep.include', {
+      // Check if the correct uuids are being sent (this might need to be adjusted)
+    });
+
+    // The users should be updated to Active
+    cy.get('table').find('tbody > tr:nth-child(1)').should('contain', 'Active');
+    cy.get('table').find('tbody > tr:nth-child(2)').should('contain', 'Active');
+
+    // A success notification should be shown
+    cy.get('body').should('contain', 'Update Successful');
+  });
+});
--- website/tests/cypress/support/commands.js
+++ website/tests/cypress/support/commands.js
@@ -17,3 +17,22 @@ Cypress.Commands.add('autoLogin', (uemail, upassword) => {
         console.log(body)
     })
 })
+
+
+Cypress.Commands.add('autoLogin', (uemail, upassword) => {
+    cy.request({
+        method: 'POST',
+        url: `${Cypress.env('API_LINK')}/auth/login`,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: {
+            email: uemail,
+            password: upassword
+        }
+    })
+        .then((resp) => {
+            // expect the server to set a cookie, which will be used in subsequent requests
+            expect(resp.status).to.eq(200);
+        });
+});
