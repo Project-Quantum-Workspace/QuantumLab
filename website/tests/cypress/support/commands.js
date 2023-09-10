9ca19c4da16ecf12455c1fa411f6f4d4f5e7a858
Xiaochen Hou <houxiaochen01@gmail.com>
2023-09-10 15:05:27 +1000

    update: finished the admin user list tests

--- website/tests/cypress/support/commands.js
+++ website/tests/cypress/support/commands.js
@@ -20,20 +20,10 @@
 
 
 
-Cypress.Commands.add('login', (email, password) => {
+Cypress.Commands.add('autoLogin', (email, password) => {
     cy.visit(`${Cypress.env('QUANTUMLAB_WEB')}`)
-    // Fill in the email and password
     cy.get('input#email').type(email);
-
-    // Assuming there's a similar input for password with an id of "password"
-    // If the ID is different, you'll need to update this selector.
     cy.get('input#password').type(password);
-
-    // Click the login button
-    // Assuming there's a button with a text "Login" or some specific selector you can use
     cy.get('button').contains('Login').click()
-
     cy.contains('Hello').should('be.visible');
-
-
 });
