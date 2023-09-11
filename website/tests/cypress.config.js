3610b3e8a67a4f632cf71deaa914b3013e1b1640
VortexMashiro <mashiro@vortexmashiro.cc>
2023-09-11 20:07:37 +1000

    ci: update cypress config

--- website/tests/cypress.config.js
+++ website/tests/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "556oje",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
