4eedf83cf0a0a4d40f9f5e33c80897bb5bedf1e3
Xiaochen Hou <houxiaochen01@gmail.com>
2023-10-10 00:18:22 +1100

    fix: added project id

--- website/cypress.config.js
+++ website/cypress.config.js
@@ -1,12 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: '556oje',
   e2e: {
     baseUrl: 'http://localhost:8000',
-    "supportFile": "cypress/support/index.js",
-    "specPattern": "cypress/e2e/*.specs.js"
+    supportFile: 'cypress/support/index.js',
+    specPattern: 'cypress/e2e/*.specs.js',
   },
-  "env":{
-    "QUANTUMLAB_WEB":"http://localhost:8000",
+  env: {
+    QUANTUMLAB_WEB: 'http://localhost:8000',
   },
 });
